738ee6ace30b2c23baae63f524477a35794c50ca
Edgar Robles <robles_edgar@icloud.com>
2025-01-13 23:48:05 -0600

    remove unused imports from Page component

--- nextjs-app/app/page.tsx
+++ nextjs-app/app/page.tsx
@@ -1,8 +1,5 @@
 import { Suspense } from "react";
-import Link from "next/link";
-
 import { AllPosts } from "@/app/components/Posts";
-import GetStartedCode from "@/app/components/GetStartedCode";
 
 export default async function Page() {
   return (
