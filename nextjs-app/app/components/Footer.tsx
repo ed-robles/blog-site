90b5bac2c778f9ca1513471b0d0233bad6ce3efb
Edgar Robles <robles_edgar@icloud.com>
2025-01-13 23:13:44 -0600

    remove .env.example file and update Footer component for improved styling

--- nextjs-app/app/components/Footer.tsx
+++ nextjs-app/app/components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="bg-gray-50 border-gray-100 border-t">
       <div className="container">
         <div className="flex flex-col items-center py-4 lg:flex-row">
-          <p className="mb-2 text-center text-xl font-bold leading-tight tracking-tighter lg:mb-0 lg:w-1/2 lg:pr-4 lg:text-left lg:text-5xl">
+          <p className="mb-2 text-center text-xl font-bold leading-tight tracking-tighter lg:mb-0 lg:w-1/2 lg:pr-4 lg:text-left">
             Built with Sanity + Next.js.
           </p>
         </div>
